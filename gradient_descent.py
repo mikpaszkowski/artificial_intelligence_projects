e354bf8bc4a7189c1f32aa26872addc4176e4a60
Sunil <sunil.ratnayake12@gmail.com>
2022-03-09 23:08:09 +0100

    optimalisationG added, gradientDescent corrected

--- gradient_descent.py
+++ gradient_descent.py
@@ -2,13 +2,16 @@ import numpy as np
 import time
 
 
-def gradientDescent(coeff, startValue, stopConditions, learnRate=0.001):
+def gradientDescent(type, coeff, startValue, stopConditions, learnRate=0.001):
     steps = [startValue]
     x = startValue
     startTime = time.time()
     for _ in range(stopConditions.maxNumOfIterations):
         print(x)
-        diff = x - learnRate * (3 * coeff.a * x * x + 2 * coeff.b * x + coeff.c)
+        if(type == "g"): #gradient descent
+            diff = learnRate*(3*coeff.a*x**2 + 2*coeff.b*x + coeff.c)
+        elif(type == "n"): #newton's formula
+            diff = learnRate * (3*coeff.a*x**2+2*coeff.b*x+coeff.c)/(6*coeff.a*x+2*coeff.b)#learnRate*(a*x**3+b*x**2+c*x+d)/(3*a*x**2+2*b*x+c)
         if np.abs(diff) < stopConditions.tolerance:
             print("a")
             break
@@ -22,6 +25,39 @@ def gradientDescent(coeff, startValue, stopConditions, learnRate=0.001):
             break
     print(x)
     return x, steps
+
+
+def optimalisationG (type, coeff, startValue, stopConditions, learnRate=0.001)):
+	startTime = time.time()
+	k = 0
+    x = startValue
+	for i in range(maxIter):
+		#print(x)
+		if(type == "g"): #gradient descent
+			diff = learnRate * (numpy.dot(2,numpy.dot(coeff.a, x))+ coeff.b)
+#			diff = learnRate * (numpy.transpose(b) + numpy.dot(a,numpy.transpose(x)) + numpy.dot(numpy.transpose(a), numpy.transpose(x))) #learnRate*(3*a*x**2 + 2*b*x + c)
+		elif(type == "n"): #newton's formula
+
+			diff =  learnRate * numpy.divide(numpy.dot(numpy.transpose(x),numpy.dot(coeff.a,x)) + numpy.dot(numpy.transpose(coeff.b),x)+c  ,numpy.dot(2,numpy.dot(coeff.a, x))+ coeff.b) 
+#			diff =  learnRate * numpy.divide(numpy.dot(2,numpy.dot(a, x))+ b ,numpy.dot(2,a)) 
+#			diff =  learnRate * numpy.divide((numpy.transpose(b) + numpy.dot(a,numpy.transpose(x)) + numpy.dot(numpy.transpose(a), numpy.transpose(x))),(a+numpy.transpose(a))) 
+#           learnRate * (c+ numpy.dot(numpy.transpose(b), x) + numpy.dot(numpy.transpose(x), numpy.dot(a, x)))/(b + numpy.dot(a,x) + numpy.dot(numpy.transpose(a), x)) #learnRate * (a*x**3+b*x**2+c*x+d)/(3*a*x**2+2*b*x+c)
+		else:
+			raise "TypeError"
+		if(numpy.all(numpy.abs(diff) <= stopConditions.tolerance)):
+			print("tolerance")
+			break
+		if(time.time()- startTime >= stopConditions.timeOut):
+			print("time out")
+			break
+		x = x - diff
+		if(numpy.all(numpy.abs(x) <= stopConditions.desiredX)):
+			print("desired x")
+			break
+		k = k+1
+	print(x)
+	print(k)
+	return x
 
 
 def gradientDescentRandom(uniformDistributionRange, coeff, stopConditions, learnRate=0.2):
