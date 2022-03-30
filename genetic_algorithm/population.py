9a5e8e601ef6478a5a493e13a90ec87056b94c02
Miki <paszkowski.mikolaj98@gmail.com>
2022-03-31 01:33:02 +0200

    minor change

--- genetic_algorithm/population.py
+++ genetic_algorithm/population.py
@@ -6,4 +6,4 @@ def generate_population(int_range, pop_size, dimension):
     for _ in range(pop_size):
         phenotype = numpy.random.randint(- 2 ** int_range, 2 ** int_range, dimension)
         population.append(phenotype)
-    return numpy.asarray(population)
+    return population
