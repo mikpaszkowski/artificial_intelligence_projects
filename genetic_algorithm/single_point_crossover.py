c1a4287767c4675a68f78d865202b39a890608b0
Sunil <sunil.ratnayake12@gmail.com>
2022-03-31 17:09:35 +0200

    single_point_crosseover andusted for multiple dimensions

--- genetic_algorithm/single_point_crossover.py
+++ genetic_algorithm/single_point_crossover.py
@@ -1,27 +1,44 @@
 import numpy
 
+def bitfield(n):
+    return [int(digit) for digit in bin(n)[2:]] # [2:] to chop off the "0b" part
 
-def intToBit(n):#https://stackoverflow.com/questions/10321978/integer-to-bitfield-as-a-list
-    return [int(digit) for digit in bin(n)[2:]] # [2:] to chop off the "0b" part 
+def intToBin(n):#https://stackoverflow.com/questions/10321978/integer-to-bitfield-as-a-list
+    ret = []
+    j = 0
+    for i in n:
+        ret.append([int(digit) for digit in bin(i)[2:]])# [2:] to chop off the "0b" part 
+        while(len(ret[j])<4):
+            ret[j].insert(0,0)
 
-def bitToInt(bitlist):#https://stackoverflow.com/questions/12461361/bits-list-to-integer-in-python
+        j=j+1
+    return  ret
+
+def binToInt(bitlist):#https://stackoverflow.com/questions/12461361/bits-list-to-integer-in-python
     out = 0
     for bit in bitlist:
         out = (out << 1) | bit
     return out
 
 def chromosomeCrossover(p1,p2):
+#    print("p1: ", p1)
+#    print("p2: ", p2)
 
     ch1_bit = []
     ch2_bit = []
+    p = []
     
     size = len(p1)
     crossover_point = numpy.random.randint(1,size)
     for i in range(size):
         if (i < crossover_point):
-            ch1_bit.append(p1[i])
+        #    print("p1[i,:]: ", p1[i,:])
+            p = p1[i]
+            ch1_bit.append(p)
+          #  print("ch1: ", ch1_bit)
             ch2_bit.append(p2[i])
         else:
+        #    print("p1[i,:]: ", p1[i,:]," p1[i]: ", p1[i])
             ch1_bit.append(p2[i])
             ch2_bit.append(p1[i])
 #    print("p1: ", p1)
@@ -29,7 +46,7 @@ def chromosomeCrossover(p1,p2):
 #    print("cross over point: ", crossover_point)
 #    print("ch1: ", ch1_bit)
 #    print("ch2: ", ch2_bit)
-
+#    print ("chromosomeCrossover output: ", ch1_bit, ch2_bit)
     return ch1_bit, ch2_bit
 
 def tuples(population):
@@ -40,14 +57,22 @@ def tuples(population):
 #        i = i+1
 #        print (i)
         p1 = population[0]
-        population.pop(0)
+#        print("pre numpy delete population:")
+#        print(population)
+        population = numpy.delete(population, 0, axis = 0) #population.pop(0)#
+#        print("post numpy delete population:")
+#        print(population)
         p2_index = numpy.random.randint(0,size-1)
         p2 = population[p2_index]
-        population.pop(p2_index)
+        population = numpy.delete(population, p2_index, axis = 0)#population.pop(p2_index)
+#        print("p1: ", p1,"\np2: ", p2)
         parents.append((p1,p2))
         size = size-2
-    
+#    print("Parent Tuples: ", parents)
     return parents
+
+
+
 
 def crossover(population, crossoverProbability):
     nextGen = []
