fb78e956d6fdf979063e327f51eb1b926f1ea547
Sunil <sunil.ratnayake12@gmail.com>
2022-04-23 23:36:40 +0200

    added getdata.py

---
+++ regression_and_classification/getdata.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+def getdata(csvname, fields):
+    data = pd.read_csv(csvname).to_numpy()
+    # print("data: ", data)
+
+    length = len(data)
+    train = [[0 for x in range(fields)] for y in range(int(length*0.8)+1)]
+    test = [[0 for x in range(fields)] for y in range(int(length*0.2))]
+
+    for i in range(length):
+        if i < length * 0.8:
+            train[i] = data[i]
+        else:
+            test[int(i-(length*0.8))][:] = data[i][:]
+    # print ("train: ", train)
+    # print ("test: ", test)
+    return train, test
