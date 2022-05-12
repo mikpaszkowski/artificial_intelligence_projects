03ff5ae94b47051f7425e6b6accef8269e03ff16
mpas <mikolaj.paszkowski@inetum.com>
2022-05-12 21:58:27 +0200

    changes in analysis, model diagnostics

--- n_layer_perceptron_classification/analysis.py
+++ n_layer_perceptron_classification/analysis.py
@@ -4,10 +4,11 @@ from sklearn.datasets import load_iris
 
 
 def analyse(dataset):
-    print(dataset.head(5))
-    sns.pairplot(data=dataset, vars=('SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm'), hue='Species')
-    dataset.hist(alpha=0.5, figsize=(20, 20), color='red')
-    dataset.plot(subplots=True, figsize=(10, 10), sharex=False, sharey=False)
+    iris = dataset.drop(columns='Id')
+    sns.pairplot(data=iris, vars=('SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm'), hue='Species')
+    iris.hist(alpha=0.5, figsize=(20, 20), color='red')
+    iris.plot(subplots=True, figsize=(10, 10), sharex=False, sharey=False)
+    plot.show()
 
     iris = load_iris()
     plot.scatter(iris.data[:, 2], iris.data[:, 3], alpha=1, c=iris.target, edgecolors='black')
@@ -15,10 +16,32 @@ def analyse(dataset):
     plot.title('Petals')
     plot.xlabel('Petal length')
     plot.ylabel('Petal width')
+    plot.show()
 
     plot.scatter(iris.data[:, 0], iris.data[:, 1], alpha=1, c=iris.target, edgecolors='black')
     plot.colorbar(ticks=([0, 1, 2]))
     plot.title('Sepals')
     plot.xlabel('Sepal length')
     plot.ylabel('Sepal width')
+    plot.legend()
+    plot.show()
+
+    iris = dataset.drop(columns='Id')
+    plot.figure(figsize=(15, 10))
+    plot.subplot(2, 2, 1)
+    sns.violinplot(x='Species', y='PetalLengthCm', data=iris)
+    plot.subplot(2, 2, 2)
+    sns.violinplot(x='Species', y='PetalWidthCm', data=iris)
+    plot.subplot(2, 2, 3)
+    sns.violinplot(x='Species', y='SepalLengthCm', data=iris)
+    plot.subplot(2, 2, 4)
+    sns.violinplot(x='Species', y='SepalWidthCm', data=iris)
+    plot.show()
+
+    plot.figure(figsize=(7, 4))
+    sns.heatmap(iris.corr(), annot=True,
+                cmap='cubehelix_r')  # draws  heatmap with input as the correlation matrix calculted by(iris.corr())
+    plot.show()
+
+    sns.FacetGrid(dataset, hue="Species", size=5).map(sns.distplot, "SepalWidthCm", hist_kws={"alpha": .2}).add_legend()
     plot.show()
--- n_layer_perceptron_classification/compare.py
+++ n_layer_perceptron_classification/compare.py
@@ -1,19 +1,20 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plot
 import timeit
-from sklearn.metrics import accuracy_score, plot_confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.neural_network import MLPClassifier
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, StandardScaler
 
+import numpy as np
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neural_network import MLPClassifier
+from model_diagnostics import validation_curve, learning_curve
+
+
+cross_entropy_loss = []
 
 
 def comparison(dataset, hidden_layers):
     ir_features = dataset.drop(columns='Species')
     ir_label = dataset['Species']
-    X_train, x_test, Y_train, y_test = train_test_split(np.array(ir_features), np.array(ir_label), test_size=0.2, random_state=10)
-
+    X_train, x_test, Y_train, y_test = train_test_split(np.array(ir_features), np.array(ir_label), test_size=0.2,
+                                                        random_state=10)
 
     accuracy_scores = []
     max_accuracy = 0
@@ -26,73 +27,81 @@ def comparison(dataset, hidden_layers):
                                 max_iter=3000, activation='relu',
                                 solver='sgd')
 
-        # standard_scaler = StandardScaler()
-        # scaler = standard_scaler.fit(X_train)
-        # trainX_scaled = scaler.transform(X_train)
-        # testX_scaled = scaler.transform(x_test)
+        validation_curve(mlp_clf, ir_features, ir_label)
+        learning_curve(mlp_clf, ir_features, ir_label)
 
         mlp_clf.fit(X_train, Y_train)
         time = timeit.default_timer() - start
-        #print(mlp_clf.score(x_test, y_test))
 
         y_pred = mlp_clf.predict(x_test)
         accuracy = accuracy_score(y_test, y_pred)
         if accuracy > max_accuracy:
             max_accuracy = accuracy
             best = layers
-        if (accuracy/time) > max_accuracy_over_time:
+        if (accuracy / time) > max_accuracy_over_time:
             max_accuracy_over_time = accuracy
             best_over_time = layers
         print("{0:0.3f}, for: ".format(accuracy), layers)
         accuracy_scores.append(accuracy)
+        # calculating cross entropy loss
+        pred = mlp_clf.predict_proba(x_test)
+        cross_entropy_loss.append(log_loss(y_test, pred))
+
+        # checking the balance
+        scores = cross_val_score(mlp_clf, X_train, Y_train, cv=3, scoring='accuracy')
+        # the samples are balanced across target classes hence the accuracy and the F1-score are almost equal.
+        f1_scores = cross_val_score(mlp_clf, X_train, Y_train, cv=3, scoring='f1_macro')
+        print("Mean Accuracy: %0.2f (+/- %0.2f) \n" % (scores.mean(), scores.std() * 2))
+
     print("best accuracy:", max_accuracy)
     print("best accuracy for: ", best)
     print("best accuracy over time: ", max_accuracy_over_time)
     print("best accuracy over time for: ", best_over_time)
+    print(cross_entropy_loss)
+
 
 def compare(dataset):
-
     hidden_layers = [
         (10),
-        (10,10),
-        (10,10,10),
-        (10,10,10,10),
-        (10,10,10,10,10),
-        (10,10,10,10,10,10),
-        (10,10,10,10,10,10,10),
-        (10,10,10,10,10,10,10,10),
-        (10,10,10,10,10,10,10,10,10),
-        (10,10,10,10,10,10,10,10,10,10)
+        (10, 10),
+        (10, 10, 10),
+        (10, 10, 10, 10),
+        (10, 10, 10, 10, 10),
+        (10, 10, 10, 10, 10, 10),
+        (10, 10, 10, 10, 10, 10, 10),
+        (10, 10, 10, 10, 10, 10, 10, 10),
+        (10, 10, 10, 10, 10, 10, 10, 10, 10),
+        (10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
     ]
 
     comparison(dataset, hidden_layers)
 
     hidden_layers = [
         (50),
-        (50,50),
-        (50,50,50),
-        (50,50,50,50),
-        (50,50,50,50,50),
-        (50,50,50,50,50,50),
-        (50,50,50,50,50,50,50),
-        (50,50,50,50,50,50,50,50),
-        (50,50,50,50,50,50,50,50,50),
-        (50,50,50,50,50,50,50,50,50,50)
+        (50, 50),
+        (50, 50, 50),
+        (50, 50, 50, 50),
+        (50, 50, 50, 50, 50),
+        (50, 50, 50, 50, 50, 50),
+        (50, 50, 50, 50, 50, 50, 50),
+        (50, 50, 50, 50, 50, 50, 50, 50),
+        (50, 50, 50, 50, 50, 50, 50, 50, 50),
+        (50, 50, 50, 50, 50, 50, 50, 50, 50, 50)
     ]
 
     comparison(dataset, hidden_layers)
 
     hidden_layers = [
         (100),
-        (100,100),
-        (100,100,100),
-        (100,100,100,100),
-        (100,100,100,100,100),
-        (100,100,100,100,100,100),
-        (100,100,100,100,100,100,100),
-        (100,100,100,100,100,100,100,100),
-        (100,100,100,100,100,100,100,100,100),
-        (100,100,100,100,100,100,100,100,100,100)
+        (100, 100),
+        (100, 100, 100),
+        (100, 100, 100, 100),
+        (100, 100, 100, 100, 100),
+        (100, 100, 100, 100, 100, 100),
+        (100, 100, 100, 100, 100, 100, 100),
+        (100, 100, 100, 100, 100, 100, 100, 100),
+        (100, 100, 100, 100, 100, 100, 100, 100, 100),
+        (100, 100, 100, 100, 100, 100, 100, 100, 100, 100)
     ]
 
     comparison(dataset, hidden_layers)
@@ -100,18 +109,18 @@ def compare(dataset):
     hidden_layers = [
         (10),
         (50),
-        (10,10),
-        (10,50),
-        (50,10),
-        (50,50),
-        (10,10,10),
-        (10,10,50),
-        (10,50,10),
-        (10,50,50),
-        (50,10,10),
-        (50,10,50),
-        (50,50,10),
-        (50,50,50)
+        (10, 10),
+        (10, 50),
+        (50, 10),
+        (50, 50),
+        (10, 10, 10),
+        (10, 10, 50),
+        (10, 50, 10),
+        (10, 50, 50),
+        (50, 10, 10),
+        (50, 10, 50),
+        (50, 50, 10),
+        (50, 50, 50)
     ]
 
     comparison(dataset, hidden_layers)
